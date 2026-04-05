f161c0037702452977b887ff54df2dcfbd63b1d3
thiagosanfarm <thiagosantanasants@gmail.com>
2026-04-05 09:55:54 -0300

    segundo dia

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 
+#loop infinito
 while True:
-	if can_harvest():
-		harvest()
-		plant(Entities.Bush)
-		move(North)
-		pet_the_piggy()
+	#rodar cada linha
+	for i in range(get_world_size()):
+		#rodar cada coluna 
+		for j in range(get_world_size()):
+			#plantar cenoura
+			if get_ground_type() == Grounds.Soil:
+				plant(Entities.Carrot)
+			if not can_harvest():
+				move(North)
+			if can_harvest():
+				harvest()
+				if get_ground_type() != Grounds.Soil: 
+					till()
+				plant(Entities.Carrot)
+			if get_water() < 0.2:
+				use_item(Items.Water)
+			move(North)
+		move(East)
+
+		
+	
 		
 		
 
